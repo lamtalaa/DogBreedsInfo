09fbe6bb69c76b508c26eb8e993e5abce442d560
Yassine Lamtalaa <ysnlmtl3@gmail.com>
2025-06-09 09:06:07 -0400

    initial commit of project

---
+++ DogBreedsInfo/DogBreedsInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedsInfoApp.swift
+//  DogBreedsInfo
+//
+//  Created by Yassine Lamtalaa on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct DogBreedsInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
